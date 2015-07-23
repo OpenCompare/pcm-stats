b203cb0d71e7aabfbf290d8d2c87d4a916b83ccc
StephaneMangin <1camel>
2015-07-23 16:59:48 +0200

    Purge MediaWikiApi and use a dependency to OpenCompare

--- src/main/java/org/opencompare/stats/Launcher.java
+++ src/main/java/org/opencompare/stats/Launcher.java
@@ -1,5 +1,7 @@
 package org.opencompare.stats;
 
+import scala.util.parsing.json.JSONObject;
+
 import java.io.*;
 
 import static scala.collection.JavaConversions.seqAsJavaList;
@@ -9,12 +11,10 @@ import static scala.collection.JavaConversions.seqAsJavaList;
  */
 public class Launcher {
 
-
     public static void main(String[] args) throws IOException {
         Launcher launcher = new Launcher();
         InputStream input = launcher.getClass().getResourceAsStream("/list_of_PCMs.csv");
         Grabber grabber = new Grabber("en", "Comparison_of_AMD_processors");
-        System.out.println(grabber.getRevIds());
         for (String revid: grabber.getRevIds()) {
             System.out.println(grabber.getDate(revid));
             System.out.println(grabber.getAuthor(revid));
--- src/main/scala/org/opencompare/stats/Grabber.scala
+++ src/main/scala/org/opencompare/stats/Grabber.scala
@@ -1,59 +1,56 @@
 package org.opencompare.stats
 
-import java.util.Date
 import java.text.DateFormat
 import java.util
-import java.util.Locale
+import java.util.{Date, Locale}
 
 import org.opencompare.io.wikipedia.io.MediaWikiAPI
 import play.api.libs.json.JsValue
 
 import scala.collection.JavaConverters._
-;
-
 /**
  * Created by smangin on 7/23/15.
  *
  * Use to get all revision from a single wikipedia page
  *
  */
-class Grabber (lang : String, title : String){
+class Grabber (lang : String, title : String) {
 
 
-    val api : MediaWikiAPI = new MediaWikiAPI("https", "wikipedia.org")
-    val result = api.getVersionFromTitle(lang, title)
+  private val api: MediaWikiAPI = new MediaWikiAPI("https", "wikipedia.org")
+  val json = api.getRevisionFromTitle(lang, title)
 
-    def getRevIds(): util.List[String] = {
-        val revids: util.List[String] = new util.ArrayList();
-        for (value: JsValue <- result) {
-            var revid = value \ "revid"
-            revids.add(revid.toString)
-        }
-        revids
+  def getRevIds(): util.List[String] = {
+    val revids: util.List[String] = new util.ArrayList();
+    for (value: JsValue <- json) {
+      var revid = value \ "revid"
+      revids.add(revid.toString)
     }
+    revids
+  }
 
-    def getDate(revid : String): Date = {
-        var timestamp = ""
-        for (value : JsValue <- result) {
-            if ((value \ "revid").equals(revid)) {
-                timestamp = (value \ "timestamp").toString
-            }
-        }
-        if (timestamp != "") {
-            val df = DateFormat.getDateInstance(DateFormat.LONG, Locale.FRANCE);
-            df.parse(timestamp)
-        } else {
-            new Date()
-        }
+  def getDate(revid: String): Date = {
+    var timestamp = ""
+    for (value: JsValue <- json) {
+      if ((value \ "revid").equals(revid)) {
+        timestamp = (value \ "timestamp").toString
+      }
     }
+    if (timestamp != "") {
+      val df = DateFormat.getDateInstance(DateFormat.LONG, Locale.FRANCE);
+      df.parse(timestamp)
+    } else {
+      new Date()
+    }
+  }
 
-    def getAuthor(revid : String): String = {
-        var author = ""
-        for (value : JsValue <- result) {
-            if ((value \ "revid").equals(revid)) {
-                author = (value \ "user").toString
-            }
-        }
-        author
+  def getAuthor(revid: String): String = {
+    var author = ""
+    for (value: JsValue <- json) {
+      if ((value \ "revid").equals(revid)) {
+        author = (value \ "user").toString
+      }
     }
+    author
+  }
 }
