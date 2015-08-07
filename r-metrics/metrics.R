cfa2d318fb3854c3e0b4d02f3344647018ae974d
StéphaneMangin <stephane.mangin@freesbee.fr>
2015-08-07 13:27:52 +0200

    Add nb matrix to the chart

--- r-metrics/metrics.R
+++ r-metrics/metrics.R
@@ -1,12 +1,12 @@
  #!/usr/bin/env Rscript
 
-#setwd("~/IdeaProjects/OpenCompare/pcm-stats/r-metrics")
-setwd(".")
+setwd("~/IdeaProjects/OpenCompare/pcm-stats/r-metrics")
+#setwd(".")
 library(DBI)
 
 con <- dbConnect(RSQLite::SQLite(), "../metrics/metrics.db")
 dbListTables(con)
-res <- dbSendQuery(con, "SELECT * FROM metrics")
+res <- dbSendQuery(con, "SELECT date, newFeatures, delFeatures, newProducts, delProducts, nbMatrices FROM metrics")
 metrics <- dbFetch(res)
 
 # Start PNG device driver to save output to figure.png
@@ -14,6 +14,8 @@ png(filename="metrics.png", height=800, width=800, bg="white")
 
 # convert column to specific type
 metrics$date <- format(as.POSIXlt(metrics$date), "%Y")
+nbMatrices <- rowsum(metrics$nbMatrices, metrics$date)
+nbMatrices
 newFeatures <- rowsum(metrics$newFeatures, metrics$date)
 newFeatures
 delFeatures <- rowsum(metrics$delFeatures, metrics$date)
@@ -24,15 +26,14 @@ delProducts <- rowsum(metrics$delProducts, metrics$date)
 delProducts
 
 # Define colors to be used
-plot_colors <- c("green", "red", "blue", "orange")
+plot_colors <- c("green", "red", "blue", "orange", "black")
 
 # get the range for the x and y axis
 daterange <- range(metrics$date)
 daterange
-yrange <- range(newProducts[,1], delProducts[,1], newFeatures[,1], delFeatures[,1])
+yrange <- range(newProducts[,1], delProducts[,1], newFeatures[,1], delFeatures[,1], nbMatrices[,1])
 yrange
-column_names <- c("New products", "Deleted products", "New features", "Deleted features")
-col <- c(7:10)
+column_names <- c("New products", "Deleted products", "New features", "Deleted features", "Nb matrices")
 
 # set up the plot
 plot(newProducts, ann=F, ylim = yrange, type="l", col=plot_colors[1], pch=21)
@@ -41,6 +42,7 @@ plot(newProducts, ann=F, ylim = yrange, type="l", col=plot_colors[1], pch=21)
 lines(delProducts, type="l", col=plot_colors[2], pch=22)
 lines(newFeatures, type="l", col=plot_colors[3], pch=23)
 lines(delFeatures, type="l", col=plot_colors[4], pch=24)
+lines(nbMatrices, type="o", col=plot_colors[5], pch=25)
 
 # add a title and subtitle
 title("Wikipedia matrices evolution", "Products and Features in matrices")
@@ -49,7 +51,7 @@ title("Wikipedia matrices evolution", "Products and Features in matrices")
 title(xlab= "Date (years)", ylab= "Nb (unit)")
 
 # add a legend
-legend("topleft", column_names, cex=0.8, col=plot_colors, pch=21:24)
+legend("topleft", column_names, cex=0.8, col=plot_colors, pch=21:25)
 
 # Turn off device driver (to flush output to png)
 dev.off()
