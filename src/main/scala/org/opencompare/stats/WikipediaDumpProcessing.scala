21984cc17a8a554a88f2d41ef6ffab95715b6e35
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-08-26 11:01:42 +0200

    cut wikipedia dump + detect tables

--- src/main/scala/org/opencompare/stats/WikipediaDumpProcessing.scala
+++ src/main/scala/org/opencompare/stats/WikipediaDumpProcessing.scala
@@ -1,14 +1,12 @@
 package org.opencompare.stats
 
-import java.io.File
-import java.nio.file.{Paths, Files}
+import java.io.{FileWriter, File}
 
-import scala.collection.mutable.ListBuffer
+import org.xml.sax.SAXParseException
+
 import scala.io.Source
-
-
-import scala.collection.JavaConversions._
-import scala.xml.XML
+import scala.xml.{XML, MetaData, NamespaceBinding}
+import scala.xml.pull._
 
 /**
  * Created by gbecan on 8/25/15.
@@ -17,154 +15,27 @@ object WikipediaDumpProcessing extends App {
 
 
   val dumpFile = new File("wikipedia-dump/zuwiki-20150806-pages-articles-multistream.xml")
+//  val dumpFile = new File("wikipedia-dump/zuwiki-20150806-pages-meta-history.xml")
+
   val outputDir = new File(dumpFile.getAbsolutePath.substring(0, dumpFile.getAbsolutePath.size - ".xml".size))
-  outputDir.mkdirs()
 
+  // Cut dump in pageId/revisionId.xml files
+//  val processor = new WikipediaDumpProcessor
+//  processor.cutDump(dumpFile, outputDir)
 
+  // Read extracted XML files and detect tables
 
-  // No parser solution
-  var inPage = false
-  var inRevision = false
-  val extraContentBuilder = ListBuffer.empty[String]
-  val pageBuilder = ListBuffer.empty[String]
-  val revisionBuilder = ListBuffer.empty[String]
+  for (pageDir <- outputDir.listFiles() if pageDir.isDirectory) {
+    for (revisionFile <- pageDir.listFiles() if revisionFile.isFile && revisionFile.getName.endsWith(".xml")) {
+      val revXml = XML.loadFile(revisionFile)
+      val revId = (revXml \ "id").text
+      val revContent = (revXml \ "text").text
+      val containsTable = revContent.contains("{|")
 
-  for (line <- Source.fromFile(dumpFile).getLines()) {
-    if (line.contains("<page>")) {
-
-      inPage = true
-      pageBuilder += line
-
-    } else if (line.contains("</page>")) {
-
-      pageBuilder += line
-      inPage = false
-
-      val page = pageBuilder.result()
-      pageBuilder.clear()
-      processPage(page)
-    } else {
-
-      if (inPage) {
-        pageBuilder += line
-      } else {
-        extraContentBuilder += line
+      if (containsTable) {
+        println(revisionFile)
       }
 
     }
   }
-
-  val extraContent = extraContentBuilder.result()
-
-  val extraContentFile = new File(outputDir.getAbsolutePath + "/extraContent.xml")
-  Files.write(extraContentFile.toPath, extraContent)
-
-
-  def processPage(page : List[String]): Unit = {
-    val pageXML = XML.loadString(page.mkString("\n"))
-    val pageId = (pageXML \ "id").text
-    val pageRevisionId = (pageXML \ "revision" \ "id").text
-
-    val pageOutputDir = new File(outputDir.getAbsolutePath + "/" + pageId)
-    pageOutputDir.mkdirs()
-
-    val pageOutputFile = new File(pageOutputDir.getAbsolutePath + "/" + pageRevisionId + ".xml")
-
-    Files.write(pageOutputFile.toPath, page)
-
-  }
-
-//  // Scales solution
-//
-//  val pullParser = pullXml(new FileInputStream(dumpFile))
-//
-//  while (pullParser.hasNext) {
-//    pullParser.next() match {
-//      case Left( i : XmlItem ) => //println(i)
-//      // do something with an XmlItem
-//      case Left( e : Elem ) if e.name.qName == "page" => println(e)
-//      // do something with start of a new element
-//      case Right(endElem) =>
-//      // do something with the end of an element
-//      case _ =>
-//    }
-//  }
-//
-//  println("processing file")
-//  val it = iterate(List(UnprefixedQName("page", Namespace("http://www.mediawiki.org/xml/export-0.10/"))), pullParser)
-//
-//  for (elem <- it) {
-//    println(elem.tree().fold())
-//  }
-
-
-//  // First solution
-//
-//  val xml = new XMLEventReader(Source.fromFile(dumpFile))
-//
-//  var insidePage = false
-//  var buf = ListBuffer[String]()
-//  for (event <- xml) {
-//    event match {
-//      case EvElemStart(_, "page", _, _) => {
-//        insidePage = true
-//        buf += backToXml(event)
-//      }
-//      case EvElemEnd(_, "page") => {
-//        buf += backToXml(event)
-//        insidePage = false
-//
-//        writePage(buf)
-//        buf.clear
-//      }
-//      case e @ EvElemStart(_, tag, _, _) => {
-//        println(e)
-//        if (insidePage) {
-//          buf += backToXml(event)
-//        }
-//      }
-//      case e @ EvElemEnd(_, tag) => {
-//        if (insidePage) {
-//          buf += backToXml(event)
-//        }
-//      }
-//      case EvText(t) => {
-//        if (insidePage) {
-//          buf += t
-//        }
-//      }
-//      case _ => // ignore
-//    }
-//  }
-//
-//  def writePage(buf: ListBuffer[String]) = {
-//    val s = buf.mkString
-//    val x = XML.loadString(s)
-//    val pageId = (x \ "id")(0).child(0).toString
-//    val f = new File(outputDir, pageId + ".xml")
-//    println("writing to: " + f.getAbsolutePath())
-//    val out = new FileOutputStream(f)
-//    out.write(s.getBytes())
-//    out.close
-//  }
-//
-//  def backToXml(ev : XMLEvent) = {
-//    ev match {
-//      case EvElemStart(pre, label, attrs, scope) => {
-//        "<" + label + attrsToString(attrs) + ">"
-//      }
-//      case EvElemEnd(pre, label) => {
-//        "</" + label + ">"
-//      }
-//      case _ => ""
-//    }
-//  }
-//
-//  def attrsToString(attrs : MetaData) = {
-//    attrs.length match {
-//      case 0 => ""
-//      case _ => attrs.map( (m:MetaData) => " " + m.key + "='" + m.value +"'" ).reduceLeft(_+_)
-//    }
-//  }
-
 }
--- src/main/scala/org/opencompare/stats/WikipediaDumpProcessor.scala
+++ src/main/scala/org/opencompare/stats/WikipediaDumpProcessor.scala
@@ -1,8 +1,106 @@
 package org.opencompare.stats
 
+import java.io.{FileWriter, File}
+
+import scala.io.Source
+import scala.xml.pull._
+
 /**
  * Created by gbecan on 8/25/15.
  */
 class WikipediaDumpProcessor {
+
+  def cutDump(dumpFile : File, outputDir : File): Unit = {
+    outputDir.mkdirs()
+
+    val sourceFile = Source.fromFile(dumpFile)
+    val xmlReader = new XMLEventReader(sourceFile)
+
+    val builder = StringBuilder.newBuilder
+
+    var inPage = false
+    var inRevision = false
+    var inId = false
+    var revisionIdDone = false
+
+    var pageId = ""
+    var revisionId = ""
+
+    for (event <- xmlReader) {
+      event match {
+        case EvElemStart(pre, label, attrs, scope) =>
+          label match {
+            case "page" => inPage = true
+            case "revision" =>
+              inRevision = true
+              revisionIdDone = false
+            case "id" if inPage => inId = true
+            case _ =>
+          }
+
+          val attributeString = if (attrs.isEmpty) {
+            ""
+          } else {
+            val attrString = for (attr <- attrs) yield {
+              val values = for (value <- attr.value) yield {
+                attr.prefixedKey + "=\"" + value.mkString + "\""
+              }
+              values.mkString(" ", " ", "")
+            }
+            attrString.mkString(" ")
+          }
+
+          if (inRevision) {
+            builder ++= "<" + label + attributeString + ">"
+          }
+
+        case EvElemEnd(pre, label) =>
+          if (inRevision) {
+            builder ++= "</" + label + ">"
+          }
+
+          label match {
+            case "page" => inPage = false
+            case "revision" =>
+              inRevision = false
+              val content = builder.result()
+              builder.clear()
+
+              val pageOutputDir = new File(outputDir.getAbsolutePath + "/" + pageId)
+              pageOutputDir.mkdirs()
+              val revisionOutputFile = new File(pageOutputDir.getAbsolutePath + "/" + revisionId + ".xml")
+
+              val writer = new FileWriter(revisionOutputFile)
+              writer.write(content)
+              writer.close()
+
+            case "id" if inRevision =>
+              inId = false
+              revisionIdDone = true
+            case "id" if inPage => inId = false
+            case _ =>
+          }
+
+        case EvEntityRef(entity) =>
+          if (inRevision) {
+            builder ++= "&" + entity + ";"
+          }
+
+        case EvProcInstr(target, text) =>
+
+        case EvText(text) =>
+          if (inId && inPage && !inRevision) {
+            pageId = text
+          } else if (inId && inRevision && !revisionIdDone) {
+            revisionId = text
+          }
+
+          if (inRevision) {
+            builder ++= text
+          }
+        case EvComment(text) =>
+      }
+    }
+  }
 
 }
