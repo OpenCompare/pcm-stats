2ad9966a95d29f17ac7a78f8ecf960512d145c5a
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-08-26 13:32:25 +0200

    extract PCMs from dump and save them in kmf + html

--- src/main/scala/org/opencompare/stats/WikipediaDumpProcessing.scala
+++ src/main/scala/org/opencompare/stats/WikipediaDumpProcessing.scala
@@ -2,13 +2,19 @@ package org.opencompare.stats
 
 import java.io.{FileWriter, File}
 
+import org.opencompare.api.java.PCMContainer
+import org.opencompare.api.java.impl.io.KMFJSONExporter
+import org.opencompare.api.java.io.HTMLExporter
 import org.opencompare.io.wikipedia.io.{MediaWikiAPI, WikiTextLoader}
 import org.opencompare.stats.utils.WikiTextKeepTemplateProcessor
-import org.xml.sax.SAXParseException
 
-import scala.io.Source
-import scala.xml.{XML, MetaData, NamespaceBinding}
-import scala.xml.pull._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+import scala.xml.XML
+
+import scala.collection.JavaConversions._
 
 /**
  * Created by gbecan on 8/25/15.
@@ -17,31 +23,68 @@ object WikipediaDumpProcessing extends App {
 
 
   val dumpFile = new File("wikipedia-dump/zuwiki-20150806-pages-articles-multistream.xml")
-//  val dumpFile = new File("wikipedia-dump/zuwiki-20150806-pages-meta-history.xml")
+  //  val dumpFile = new File("wikipedia-dump/zuwiki-20150806-pages-meta-history.xml")
 
   val outputDir = new File(dumpFile.getAbsolutePath.substring(0, dumpFile.getAbsolutePath.size - ".xml".size))
 
   // Cut dump in pageId/revisionId.xml files
-//  val processor = new WikipediaDumpProcessor
-//  processor.cutDump(dumpFile, outputDir)
+  //  val processor = new WikipediaDumpProcessor
+  //  processor.cutDump(dumpFile, outputDir)
 
   // Read extracted XML files, detect tables and extract PCMs
 
   val api = new MediaWikiAPI("wikipedia.org")
-  val pcmMiner = new WikiTextLoader(new WikiTextKeepTemplateProcessor(api))
 
-  for (pageDir <- outputDir.listFiles() if pageDir.isDirectory) {
-    for (revisionFile <- pageDir.listFiles() if revisionFile.isFile && revisionFile.getName.endsWith(".xml")) {
-      val revXml = XML.loadFile(revisionFile)
-      val revId = (revXml \ "id").text
-      val revContent = (revXml \ "text").text
-      val containsTable = revContent.contains("{|")
+  val jobs = (for (pageDir <- outputDir.listFiles() if pageDir.isDirectory) yield {
+    for (revisionFile <- pageDir.listFiles() if revisionFile.isFile && revisionFile.getName.endsWith(".xml")) yield {
+      val job = Future {
+        val revXml = XML.loadFile(revisionFile)
+        val revId = (revXml \ "id").text
+        val revContent = (revXml \ "text").text
+        val containsTable = revContent.contains("{|")
 
-      if (containsTable) {
-        val pcmContainers = pcmMiner.load(revContent)
-        println(pcmContainers.size())
+        if (containsTable) {
+          val pcmMiner = new WikiTextLoader(new WikiTextKeepTemplateProcessor(api))
+          val kmfExporter = new KMFJSONExporter
+          val htmlExporter = new HTMLExporter
+          val pcmContainers = pcmMiner.load(revContent)
+          val pcmFileBasePath = revisionFile.getAbsolutePath.substring(0, revisionFile.getAbsolutePath.size - ".xml".size)
+
+          // Write PCMs
+          for ((pcmContainer, index) <- pcmContainers.zipWithIndex) {
+            val kmfWriter = new FileWriter(pcmFileBasePath + "_" + index + ".pcm")
+            kmfWriter.write(kmfExporter.export(pcmContainer))
+            kmfWriter.close()
+
+            val htmlWriter = new FileWriter(pcmFileBasePath + "_" + index + ".html")
+            htmlWriter.write(htmlExporter.export(pcmContainer))
+            htmlWriter.close()
+          }
+
+          Some(revisionFile, pcmContainers.size())
+        } else {
+          None
+        }
       }
 
+      job.onSuccess {
+        case Some(result) => println(result._1.getAbsolutePath + " = " + result._2)
+        case None =>
+      }
+
+      job.onFailure {
+        case e => e.printStackTrace()
+      }
+
+      job
     }
-  }
+  }).flatten.toList
+
+
+  val aggregatedJobs = Future.sequence(jobs)
+
+  Await.result(aggregatedJobs, 10 minutes)
+
+
 }
+
